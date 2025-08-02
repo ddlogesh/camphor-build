f47b0d506bded6201bde76cf0403cad3043af040
Logesh Dinakaran <ddlogesh@gmail.com>
2025-08-02 06:28:57 +0530

    Add support for custom build and git clone

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# camphor-build
-Build script of Camphor dependencies
+# Camphor Build
+
+Build script to clone the dependencies if not exist and install them into `~/.camphor` directory.
+
+# Instructions
+
+```shell
+# Build WASM-compatible libraries of all projects 
+./build-wasm.sh
+
+# Build GCC-compatible (or native) libraries of all projects 
+./build-wasm.sh --native
+
+# Build sqlite amalgamation
+./build-wasm.sh --sqlite
+
+# Build WASM-compatible library of xlsxio and libexpat  
+./build-wasm.sh --xlsxio --libexpat
+```
--- build_wasm.sh
+++ build_wasm.sh
@@ -5,8 +5,26 @@ WORKDIR=$PWD
 CAMPHOR_HOME=$HOME/.camphor
 
 BUILD_TYPE="wasm"
-if [[ "$1" == "--native" ]]; then
-  BUILD_TYPE="native"
+PROJECT_FLAGS=()
+ALL_PROJECTS=(zlib-ng minizip-ng libexpat xlsxio sqlite)
+
+for arg in "$@"; do
+  case $arg in
+    --native)
+      BUILD_TYPE="native"
+      ;;
+    --*)
+      PROJECT_FLAGS+=("${arg:2}")
+      ;;
+    *)
+      echo "Unknown argument: $arg"
+      exit 1
+      ;;
+  esac
+done
+
+if [ ${#PROJECT_FLAGS[@]} -eq 0 ]; then
+  PROJECT_FLAGS=("${ALL_PROJECTS[@]}")
 fi
 
 if [[ "$BUILD_TYPE" == "wasm" ]]; then
@@ -25,75 +43,109 @@ COMMON_CMAKE_FLAGS=(
   -DCMAKE_BUILD_TYPE=Release
 )
 
-# zlib-ng
-cd "$WORKDIR/zlib-ng"
-rm -rf build
-mkdir build && cd build
+REPOS="
+zlib-ng https://github.com/ddlogesh/zlib-ng.git
+minizip-ng https://github.com/ddlogesh/minizip-ng.git
+libexpat https://github.com/ddlogesh/libexpat.git
+xlsxio https://github.com/ddlogesh/xlsxio.git
+sqlite https://github.com/ddlogesh/sqlite.git
+"
 
-$CMAKE_CMD .. \
-  "${COMMON_CMAKE_FLAGS[@]}" \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DZLIB_COMPAT=ON \
-  -DZLIB_ENABLE_TESTS=OFF \
-  -DWITH_GTEST=OFF
-$MAKE_CMD -j8
-$MAKE_CMD install
+# Clone repositories if not exist
+echo "$REPOS" | while read -r name url; do
+  if [ ! -d "$WORKDIR/$name" ]; then
+    echo "Cloning $name..."
+    git clone "$url" "$WORKDIR/$name"
+  fi
+done
 
-# minizip-ng
-cd "$WORKDIR/minizip-ng"
-rm -rf build
-mkdir build && cd build
+build_zlib_ng() {
+  cd "$WORKDIR/zlib-ng"
+  rm -rf build
+  mkdir build && cd build
 
-$CMAKE_CMD .. \
-  "${COMMON_CMAKE_FLAGS[@]}" \
-  -DBUILD_SHARED_LIBS=OFF \
-  -DMZ_COMPAT=ON \
-  -DMZ_FETCH_LIBS=OFF \
-  -DMZ_ZLIB=ON \
-  -DMZ_BZIP2=OFF \
-  -DMZ_LZMA=OFF \
-  -DMZ_ZSTD=OFF \
-  -DMZ_LIBCOMP=OFF \
-  -DMZ_OPENSSL=OFF \
-  -DMZ_PKCRYPT=OFF \
-  -DMZ_WZAES=OFF \
-  -DMZ_LIBBSD=OFF \
-  -DMZ_ICONV=OFF
-$MAKE_CMD -j8
-$MAKE_CMD install
+  $CMAKE_CMD .. \
+    "${COMMON_CMAKE_FLAGS[@]}" \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DZLIB_COMPAT=ON \
+    -DZLIB_ENABLE_TESTS=OFF \
+    -DWITH_GTEST=OFF
+  $MAKE_CMD -j8
+  $MAKE_CMD install
+}
 
-# expat
-cd "$WORKDIR/libexpat/expat"
-rm -rf build
-mkdir build && cd build
+build_minizip_ng() {
+  cd "$WORKDIR/minizip-ng"
+  rm -rf build
+  mkdir build && cd build
 
-$CMAKE_CMD .. \
-  "${COMMON_CMAKE_FLAGS[@]}" \
-  -DEXPAT_BUILD_DOCS=OFF \
-  -DEXPAT_BUILD_EXAMPLES=OFF \
-  -DEXPAT_BUILD_TESTS=OFF \
-  -DEXPAT_SHARED_LIBS=OFF
-$MAKE_CMD -j8
-$MAKE_CMD install
+  $CMAKE_CMD .. \
+    "${COMMON_CMAKE_FLAGS[@]}" \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DMZ_COMPAT=ON \
+    -DMZ_FETCH_LIBS=OFF \
+    -DMZ_ZLIB=ON \
+    -DMZ_BZIP2=OFF \
+    -DMZ_LZMA=OFF \
+    -DMZ_ZSTD=OFF \
+    -DMZ_LIBCOMP=OFF \
+    -DMZ_OPENSSL=OFF \
+    -DMZ_PKCRYPT=OFF \
+    -DMZ_WZAES=OFF \
+    -DMZ_LIBBSD=OFF \
+    -DMZ_ICONV=OFF
+  $MAKE_CMD -j8
+  $MAKE_CMD install
+}
 
-# xlsxio
-cd "$WORKDIR/xlsxio"
-rm -rf build
-mkdir build && cd build
+build_libexpat() {
+  cd "$WORKDIR/libexpat/expat"
+  rm -rf build
+  mkdir build && cd build
 
-$CMAKE_CMD .. \
-  "${COMMON_CMAKE_FLAGS[@]}" \
-  -DBUILD_STATIC=ON \
-  -DBUILD_SHARED=OFF \
-  -DBUILD_TOOLS=OFF \
-  -DBUILD_EXAMPLES=OFF
-$MAKE_CMD -j8
-$MAKE_CMD install
+  $CMAKE_CMD .. \
+    "${COMMON_CMAKE_FLAGS[@]}" \
+    -DEXPAT_BUILD_DOCS=OFF \
+    -DEXPAT_BUILD_EXAMPLES=OFF \
+    -DEXPAT_BUILD_TESTS=OFF \
+    -DEXPAT_SHARED_LIBS=OFF
+  $MAKE_CMD -j8
+  $MAKE_CMD install
+}
 
-# sqlite
-cd "$WORKDIR/sqlite"
-rm -rf build
-mkdir build && cd build
+build_xlsxio() {
+  cd "$WORKDIR/xlsxio"
+  rm -rf build
+  mkdir build && cd build
 
-../configure
-$MAKE_CMD sqlite3.c
+  $CMAKE_CMD .. \
+    "${COMMON_CMAKE_FLAGS[@]}" \
+    -DBUILD_STATIC=ON \
+    -DBUILD_SHARED=OFF \
+    -DBUILD_TOOLS=OFF \
+    -DBUILD_EXAMPLES=OFF
+  $MAKE_CMD -j8
+  $MAKE_CMD install
+}
+
+build_sqlite() {
+  cd "$WORKDIR/sqlite"
+  rm -rf build
+  mkdir build && cd build
+
+  ../configure
+  $MAKE_CMD sqlite3.c
+}
+
+for project in "${PROJECT_FLAGS[@]}"; do
+  case "$project" in
+    zlib-ng)      build_zlib_ng ;;
+    minizip-ng)   build_minizip_ng ;;
+    libexpat)     build_libexpat ;;
+    xlsxio)       build_xlsxio ;;
+    sqlite)       build_sqlite ;;
+    *)
+      echo "Warning: Unknown flag --$project (ignored)"
+      ;;
+  esac
+done
